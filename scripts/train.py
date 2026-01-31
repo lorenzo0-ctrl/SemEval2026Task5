95a132703a64bef792633a81e32b4525bb5d69aa
Lorenzo De Marco <lorenzo.demarco002@gmail.com>
2026-01-31 19:59:21 +0100

    final setup

--- scripts/train.py
+++ scripts/train.py
@@ -46,7 +46,8 @@ def main(args):
         report_to="none", 
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
-        remove_unused_columns=False
+        remove_unused_columns=False,
+        weight_decay=0.01
     )
     
     # 5. Initialize Custom Trainer
